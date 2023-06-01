41eb8210884fd99fbe09036a882752d2770ab8d3
Alphonse Bonnin <126913524+alelvis@users.noreply.github.com>
2023-06-01 00:03:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hell_world():
   return "YoloSwag"
+
+@app.route("/about")
+def about():
+  return "Alphonse e bonitao"
+
+@app.route("/contact")
+def contact():
+  return "We here bro"
